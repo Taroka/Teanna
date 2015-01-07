b0b2157273c5a645fee3e74576ace629667eeb75
Taroka <kiy535353@hotmail.com>
2015-01-06 23:07:49 -0800

    first cup of tea

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Teanna::Application.config.secret_key_base = '13017eec6857611d5fca880c8c0cea34428da17fb129a62f4ad18a85e8cf52bd537c2a1539a3c9d312d3a4f97d45316e52cfb023ae048ed2665b1a6d1946e442'
